913a8f5fc988d4d9d00c4fdb75743bf2b4520174
Karl <Karl_Harmer@hotmail.com>
2022-06-14 14:50:40 +0100

    Optimize code

--- player.lua
+++ player.lua
@@ -11,12 +11,21 @@ end
 
 
 function Player:update(dt)
+    self:move(dt)
+    self:checkBoundaries()
+end
+
+
+function Player:move(dt)
     if love.keyboard.isDown("w") then
         self.y = self.y - self.speed * dt
     elseif love.keyboard.isDown("s") then
         self.y = self.y + self.speed * dt
     end
+end
 
+
+function Player:checkBoundaries()
     if self.y < 0 then
         self.y = 0
     elseif self.y + self.height > love.graphics.getHeight() then
